28804b09fa1212f386179d31b0f3193aebaf9ed2
Vika <vika.tulnikova@bk.ru>
2023-01-26 11:33:07 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите значение b1 = ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число k1 = ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b2 = ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число k2 = ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"Две прямые пересекутся в точке с координатами: ({x}; {y})");
+
